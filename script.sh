1d73bd5bd88ad4abd852e3cc0ede7cc54a1c06d3
esioDesynC <esio_paiva@hotmail.com>
2022-12-20 20:08:57 -0300

    Adicionando arquivos de configuração

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!bin/bash/
+
+echo "Build das imagens ......." 
+
+docker build -t esiodesync/projeto-backend:1.0 backend/.
+docker build -t esiodesync/projeto-backend:1.0 database/.
+
+
+echo "Envio para o DockerHub das imagens....."
+
+docker push esiodesync/projeto-backend:1.0
+docker push esiodesync/projeto-database:1.0
+
+echo "Criando os serviços do cluster Kubernetes ......"
+
+kubectl apply -f ./services.yml
+
+
+echo "Realizando o Deployment......"
+
+kubectl apply -f ./deployment.yml
